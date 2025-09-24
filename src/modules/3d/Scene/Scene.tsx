099bc4ee8121fc53a9c785bcbf57b8986bfa44f3
italobarrosme <italobarros.me@gmail.com>
2025-09-24 15:52:28 -0300

    feat: update start script and add dog model to 3D scene

--- src/modules/3d/Scene/Scene.tsx
+++ src/modules/3d/Scene/Scene.tsx
@@ -16,6 +16,7 @@ import { Closet } from '../pieces/closet/closet'
 import { Case } from '../pieces/case/case'
 import { Bed } from '../pieces/bed/bed'
 import { Html } from '@react-three/drei'
+import { Dog } from '../pieces/dog/dog'
 
 type SceneProps = {
   gameData: {
@@ -37,6 +38,7 @@ export const Scene = ({ gameData }: SceneProps) => {
     resetGameSelection,
     getSelectedGamePosition,
   } = useChooseGames()
+
   const { boxes } = gameData
 
   if (!boxes) return null
@@ -82,6 +84,9 @@ export const Scene = ({ gameData }: SceneProps) => {
             <RigidBody>
               <Closet position={[9, 0, 5]} rotation={[0, 11, 0]} />
             </RigidBody>
+            <RigidBody>
+              <Dog position={[9, 12, 5]} rotation={[0, 11, 0]} />
+            </RigidBody>
             {boxes.map((box: BoxData, index: number) => {
               const isSelected = selectedGame?.id === box.id
               const targetPosition: [number, number, number] = isSelected
@@ -100,63 +105,63 @@ export const Scene = ({ gameData }: SceneProps) => {
                 />
               )
             })}
-          </Physics>
-          <Html
-            transform
-            position={[-7, 10, -10]}
-            style={{
-              pointerEvents: 'all',
-              userSelect: 'none',
-            }}
-          >
-            <section className="flex flex-col justify-center gap-4 bg-game-info-bg-primary max-w-96 p-4">
-              <h1 className="text-4xl font-bold text-game-info-text-primary">
-                Bem-vindo! Explore os jogos disponíveis nos cases espalhados
-                pelo quarto
-              </h1>
-              <p className="text-game-info-text-secondary text-lg">
-                Use o mouse para navegar pelo quarto e selecionar os jogos.
-              </p>
-              <ul className=" text-game-info-text-secondary text-lg">
-                <li>
-                  <strong>Zoom:</strong> scroll do mouse
-                </li>
-                <li>
-                  <strong>Rotacionar:</strong> pressionar o botão esquerdo do
-                  mouse
-                </li>
-                <li>
-                  <strong>Mover:</strong> pressionar o botão direito do mouse
-                </li>
-              </ul>
-            </section>
-          </Html>
-
-          {/* Plano de informações do jogo selecionado */}
-          {selectedGame && showInfo && (
-            <GameInfoPlane
-              position={[0, 5, 10]} // Posição calculada dinamicamente
-              visible={showInfo}
+            <Html
+              transform
+              position={[-7, 10, -10]}
+              style={{
+                pointerEvents: 'all',
+                userSelect: 'none',
+              }}
             >
-              {/* Conteúdo HTML sobreposto */}
-              <Html
-                transform
-                occlude
-                position={[0, 0, 0.01]}
-                style={{
-                  width: '380px',
-                  height: '640px',
-                  pointerEvents: 'all',
-                  userSelect: 'none',
-                }}
+              <section className="flex flex-col justify-center gap-4 bg-game-info-bg-primary max-w-96 p-4">
+                <h1 className="text-4xl font-bold text-game-info-text-primary">
+                  Bem-vindo! Explore os jogos disponíveis nos cases espalhados
+                  pelo quarto
+                </h1>
+                <p className="text-game-info-text-secondary text-lg">
+                  Use o mouse para navegar pelo quarto e selecionar os jogos.
+                </p>
+                <ul className=" text-game-info-text-secondary text-lg">
+                  <li>
+                    <strong>Zoom:</strong> scroll do mouse
+                  </li>
+                  <li>
+                    <strong>Rotacionar:</strong> pressionar o botão esquerdo do
+                    mouse
+                  </li>
+                  <li>
+                    <strong>Mover:</strong> pressionar o botão direito do mouse
+                  </li>
+                </ul>
+              </section>
+            </Html>
+
+            {/* Plano de informações do jogo selecionado */}
+            {selectedGame && showInfo && (
+              <GameInfoPlane
+                position={[0, 5, 10]} // Posição calculada dinamicamente
+                visible={showInfo}
               >
-                <GameInfoUI
-                  gameData={selectedGame}
-                  onClose={resetGameSelection}
-                />
-              </Html>
-            </GameInfoPlane>
-          )}
+                {/* Conteúdo HTML sobreposto */}
+                <Html
+                  transform
+                  occlude
+                  position={[0, 0, 0.01]}
+                  style={{
+                    width: '380px',
+                    height: '640px',
+                    pointerEvents: 'all',
+                    userSelect: 'none',
+                  }}
+                >
+                  <GameInfoUI
+                    gameData={selectedGame}
+                    onClose={resetGameSelection}
+                  />
+                </Html>
+              </GameInfoPlane>
+            )}
+          </Physics>
         </group>
       </Selection>
     </>
