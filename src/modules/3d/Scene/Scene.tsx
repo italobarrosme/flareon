6cec1895de88507295ffa8afcd19b465a642fe6c
italobarrosme <italobarros.me@gmail.com>
2025-09-24 15:30:45 -0300

    feat: update 3D scene with new furniture models and game info display

--- src/modules/3d/Scene/Scene.tsx
+++ src/modules/3d/Scene/Scene.tsx
@@ -2,14 +2,20 @@
 
 import { Physics, RigidBody } from '@react-three/rapier'
 import { AnimatedBoxGame } from '@/modules/games/boxGame/components'
-import { GameInfoPlane } from '@/modules/games/components'
+import { GameInfoPlane, GameInfoUI } from '@/modules/games/components'
 import { useChooseGames } from '@/modules/games/hooks/useChooseGames'
 import { BoxData } from '@/modules/games/boxGame/type'
 import { ThreeEvent } from '@react-three/fiber'
 import { EffectComposer, Outline } from '@react-three/postprocessing'
 import { useRef } from 'react'
 import { Selection, Select } from '@react-three/postprocessing'
-import { Pointer } from '../animations/components/Pointer/Pointer'
+import { Chair } from '../pieces/chair/chair'
+import { Sofa } from '../pieces/Sofa'
+import { DeskTable } from '../pieces/desk-table/desk-table'
+import { Closet } from '../pieces/closet/closet'
+import { Case } from '../pieces/case/case'
+import { Bed } from '../pieces/bed/bed'
+import { Html } from '@react-three/drei'
 
 type SceneProps = {
   gameData: {
@@ -29,7 +35,6 @@ export const Scene = ({ gameData }: SceneProps) => {
     selectedGame,
     showInfo,
     resetGameSelection,
-    getInfoPlanePosition,
     getSelectedGamePosition,
   } = useChooseGames()
   const { boxes } = gameData
@@ -59,6 +64,24 @@ export const Scene = ({ gameData }: SceneProps) => {
                 </mesh>
               </Select>
             </RigidBody>
+            <RigidBody>
+              <Chair position={[6, 0, -5]} rotation={[0, -11, 0]} />
+            </RigidBody>
+            <RigidBody>
+              <Bed position={[-8, 0, -6]} rotation={[0, 0, 0]} />
+            </RigidBody>
+            <RigidBody>
+              <Case position={[9, 10, -1.1]} rotation={[0, 11, 0]} />
+            </RigidBody>
+            <RigidBody>
+              <DeskTable position={[8, 0, -5]} rotation={[0, 11, 0]} />
+            </RigidBody>
+            <RigidBody>
+              <Sofa position={[-7.5, 0, 5]} rotation={[0, -11, 0]} />
+            </RigidBody>
+            <RigidBody>
+              <Closet position={[9, 0, 5]} rotation={[0, 11, 0]} />
+            </RigidBody>
             {boxes.map((box: BoxData, index: number) => {
               const isSelected = selectedGame?.id === box.id
               const targetPosition: [number, number, number] = isSelected
@@ -77,17 +100,62 @@ export const Scene = ({ gameData }: SceneProps) => {
                 />
               )
             })}
-            <Pointer />
           </Physics>
+          <Html
+            transform
+            position={[-7, 10, -10]}
+            style={{
+              pointerEvents: 'all',
+              userSelect: 'none',
+            }}
+          >
+            <section className="flex flex-col justify-center gap-4 bg-game-info-bg-primary max-w-96 p-4">
+              <h1 className="text-4xl font-bold text-game-info-text-primary">
+                Bem-vindo! Explore os jogos disponíveis nos cases espalhados
+                pelo quarto
+              </h1>
+              <p className="text-game-info-text-secondary text-lg">
+                Use o mouse para navegar pelo quarto e selecionar os jogos.
+              </p>
+              <ul className=" text-game-info-text-secondary text-lg">
+                <li>
+                  <strong>Zoom:</strong> scroll do mouse
+                </li>
+                <li>
+                  <strong>Rotacionar:</strong> pressionar o botão esquerdo do
+                  mouse
+                </li>
+                <li>
+                  <strong>Mover:</strong> pressionar o botão direito do mouse
+                </li>
+              </ul>
+            </section>
+          </Html>
 
           {/* Plano de informações do jogo selecionado */}
           {selectedGame && showInfo && (
             <GameInfoPlane
-              gameData={selectedGame}
-              position={getInfoPlanePosition()} // Posição calculada dinamicamente
-              onClose={resetGameSelection}
+              position={[0, 5, 10]} // Posição calculada dinamicamente
               visible={showInfo}
-            />
+            >
+              {/* Conteúdo HTML sobreposto */}
+              <Html
+                transform
+                occlude
+                position={[0, 0, 0.01]}
+                style={{
+                  width: '380px',
+                  height: '640px',
+                  pointerEvents: 'all',
+                  userSelect: 'none',
+                }}
+              >
+                <GameInfoUI
+                  gameData={selectedGame}
+                  onClose={resetGameSelection}
+                />
+              </Html>
+            </GameInfoPlane>
           )}
         </group>
       </Selection>
--- src/modules/3d/pieces/desk-table/desk-table.tsx
+++ src/modules/3d/pieces/desk-table/desk-table.tsx
@@ -2,18 +2,23 @@ import { useGLTF } from '@react-three/drei'
 import { GLTFNode, FurnitureProps } from '../../types/gltf-types'
 
 export function DeskTable(props: FurnitureProps) {
-  const { nodes, materials } = useGLTF('/desk-table.glb')
-  const deskTableNode = nodes['desk-table'] as GLTFNode
+  const { nodes, materials } = useGLTF('/desk-table/desk-table.glb')
+  const deskTableNode = nodes.model as GLTFNode
+
+  console.log(deskTableNode)
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={deskTableNode.geometry}
-        material={materials['desk-table']}
-      />
+        material={materials.model}
+        scale={5}
+      >
+        <meshStandardMaterial color="#5D688A" />
+      </mesh>
     </group>
   )
 }
 
-useGLTF.preload('/desk-table.glb')
+useGLTF.preload('/desk-table/desk-table.glb')
